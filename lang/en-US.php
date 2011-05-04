f5990045639869283695064c8cb2ea76069d8369
eazymatch <eazymatch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-04 08:15:47 +0000

    application translated to english
    git-svn-id: https://plugins.svn.wordpress.org/eazymatch/trunk@380604 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lang/en-US.php
+++ lang/en-US.php
@@ -1,170 +1,170 @@
 <?php 
     /** widgets cms **/
-    define('EMOL_WIDGET_LOGIN'            ,'Inloggen');
-    define('EMOL_WIDGET_TOP5'            ,'Top 5 Banen');
-    define('EMOL_WIDGET_SEARCH'         ,'Zoeken');
-    define('EMOL_WIDGET_FREE_SEARCH'    ,'Vrij zoeken');
+    define('EMOL_WIDGET_LOGIN'            ,'Login');
+    define('EMOL_WIDGET_TOP5'            ,'Top 5 Jobs');
+    define('EMOL_WIDGET_SEARCH'         ,'Search');
+    define('EMOL_WIDGET_FREE_SEARCH'    ,'Search');
     
     /**jobsearch **/
-    define('EMOL_JOBSEARCH_TOTAL'         ,'Totaal aantal vacatures: ');
-    define('EMOL_JOBSEARCH_MORE'         ,'Meer vacatures? klik hier. ');
-    define('EMOL_JOBSEARCH_APPLY'         ,'direct solliciteren ');
+    define('EMOL_JOBSEARCH_TOTAL'         ,'Total amount jobs: ');
+    define('EMOL_JOBSEARCH_MORE'         ,'More jobs? click here. ');
+    define('EMOL_JOBSEARCH_APPLY'         ,'Apply ');
     
     /**jobcvsearch **/
-    define('EMOL_CVSEARCH_TOTAL'         ,'Totaal aantal cv\'s: ');
-    define('EMOL_CVSEARCH_PAGE'         ,'Pagina: ');
-    define('EMOL_CVSEARCH_MORE'         ,'Meer CV\'s? klik hier. ');
-    define('EMOL_CVSEARCH_APPLY'         ,'meer informatie aanvragen');
+    define('EMOL_CVSEARCH_TOTAL'         ,'Total amount of resume\'s: ');
+    define('EMOL_CVSEARCH_PAGE'         ,'Page: ');
+    define('EMOL_CVSEARCH_MORE'         ,'More resume\'s? Click here. ');
+    define('EMOL_CVSEARCH_APPLY'         ,'ask for more info.');
     
     
     /**job page**/
-    define('EMOL_JOB_COMPETENCES'         ,'Eigenschappen van deze vacature ');
-    define('EMOL_JOB_NAME'              ,'Vacature ');
+    define('EMOL_JOB_COMPETENCES'         ,'Features of this job ');
+    define('EMOL_JOB_NAME'              ,'Job ');
     define('EMOL_JOB_CODE'                 ,'Code ');
-    define('EMOL_JOB_APPLY_FREE'         ,'Open inschrijving ');
+    define('EMOL_JOB_APPLY_FREE'         ,'Open enrollment ');
     define('EMOL_JOB_PICTURE'             ,'&nbsp; ');
-    define('EMOL_JOB_PLACE'             ,'Regio ');
-    define('EMOL_JOB_DATE'                ,'Datum vacature ');
-    define('EMOL_JOB_DESCRIPTION'         ,'Omschrijving ');
-    define('EMOL_JOB_APPLY'             ,'Solliciteer ');
-    define('EMOL_JOB_START'             ,'Startdatum ');
-    define('EMOL_JOB_END'                 ,'Einddatum ');
+    define('EMOL_JOB_PLACE'             ,'Region ');
+    define('EMOL_JOB_DATE'                ,'Date ');
+    define('EMOL_JOB_DESCRIPTION'         ,'Description ');
+    define('EMOL_JOB_APPLY'             ,'Apply ');
+    define('EMOL_JOB_START'             ,'Startdate ');
+    define('EMOL_JOB_END'                 ,'Enddate ');
     define('EMOL_JOB_STATE'             ,'Status ');
     
     /**cv page**/
-    define('EMOL_CV_NAME'                 ,'CV ');
-    define('EMOL_CV_DESCRIPTION'         ,'Omschrijving kandidaat');
+    define('EMOL_CV_NAME'                 ,'Resume ');
+    define('EMOL_CV_DESCRIPTION'         ,'Description');
     define('EMOL_CV_PICTURE'             ,'&nbsp;');
-    define('EMOL_CV_MATCHPROFILE'         ,'Match profiel');
-    define('EMOL_CV_PLACE'                 ,'Woonplaats');
-    define('EMOL_CV_BIRTHDATE'             ,'Geboortejaar');
+    define('EMOL_CV_MATCHPROFILE'         ,'Match profile');
+    define('EMOL_CV_PLACE'                 ,'Place');
+    define('EMOL_CV_BIRTHDATE'             ,'Date of birth');
     define('EMOL_CV_DIRECT'             ,'direct');
-    define('EMOL_CV_AVAILABLEFROM'         ,'Beschikbaar vanaf');
-    define('EMOL_CV_REACT_FREE'         ,'Open aanmelding');
+    define('EMOL_CV_AVAILABLEFROM'         ,'Available from');
+    define('EMOL_CV_REACT_FREE'         ,'Open registration');
     
     /**react to cv / companysubscription**/
-    define('EMOL_REACT_FAIL_MSG'         ,'Uw aamelding is mislukt. Neem contact op met onze helpdesk.');
-    define('EMOL_REACT_COMPANY'         ,'Bedrijfsnaam');
-    define('EMOL_REACT_JOB'             ,'Welke functie biedt u aan?');
-    define('EMOL_REACT_LOGO'             ,'Bedrijfslogo (min. 200px breed)');
-    define('EMOL_REACT_JOBDOC'             ,'Vacature document');
-    define('EMOL_REACT_CITY'             ,'Plaats');
-    define('EMOL_REACT_MESSAGE'         ,'Details van uw aanvraag');
-    define('EMOL_REACT_SEND'             ,'Aanvraag versturen');
-    define('EMOL_REACT_BEHALF'             ,'namens');
-    define('EMOL_REACT_COC'             ,'KVK');
-    define('EMOL_REACT_DEPARTMENT'        ,'Afdeling');
-    define('EMOL_REACT_BPROFILE'        ,'Bedrijfsprofiel');
+    define('EMOL_REACT_FAIL_MSG'         ,'Your registration failed. Contact our helpdesk.');
+    define('EMOL_REACT_COMPANY'         ,'Companyname');
+    define('EMOL_REACT_JOB'             ,'What job do you have?');
+    define('EMOL_REACT_LOGO'             ,'Company logo (min. 200px wide)');
+    define('EMOL_REACT_JOBDOC'             ,'Job document');
+    define('EMOL_REACT_CITY'             ,'Place');
+    define('EMOL_REACT_MESSAGE'         ,'Details of your request');
+    define('EMOL_REACT_SEND'             ,'Send inquiry');
+    define('EMOL_REACT_BEHALF'             ,'behalfe');
+    define('EMOL_REACT_COC'             ,'CoC');
+    define('EMOL_REACT_DEPARTMENT'        ,'Department');
+    define('EMOL_REACT_BPROFILE'        ,'Company profile');
     define('EMOL_REACT_BRANCHE'            ,'Branche');
-    define('EMOL_REACT_SIZE'            ,'Bedrijfsgrootte');
+    define('EMOL_REACT_SIZE'            ,'Companysize');
     
     /** company menu **/
-    define('EMOL_MENU_COMP_NAW'            ,'Bedrijfsgegevens');
-    define('EMOL_MENU_COMP_JOBS'        ,'Vacatures');
-    define('EMOL_MENU_COMP_APLIC'        ,'Sollicitaties &amp; uitnodigingen');
+    define('EMOL_MENU_COMP_NAW'            ,'Company Info');
+    define('EMOL_MENU_COMP_JOBS'        ,'Jobs');
+    define('EMOL_MENU_COMP_APLIC'        ,'Applications &amp; Invitations');
     
     /** application /mediation **/
-    define('EMOL_APPL_DATE'                ,'Datum gewijzigd');
-    define('EMOL_APPL_APPTITLE'            ,'Kandidaat');
-    define('EMOL_APPL_JOB'                ,'Vacature');
+    define('EMOL_APPL_DATE'                ,'Date changed');
+    define('EMOL_APPL_APPTITLE'            ,'Candidate');
+    define('EMOL_APPL_JOB'                ,'Job');
     define('EMOL_APPL_STATE'            ,'Status');
     
     /** applicant menu **/
-    define('EMOL_MENU_APP_NAW'            ,'NAW gegevens');
-    define('EMOL_MENU_APP_CV'            ,'CV');
-    define('EMOL_MENU_APP_APLIC'        ,'Sollicitaties &amp; uitnodigingen');
-    define('EMOL_MENU_APP_MATCH'        ,'Match profiel (mijn wensen)');
+    define('EMOL_MENU_APP_NAW'            ,'Address details');
+    define('EMOL_MENU_APP_CV'            ,'Resume');
+    define('EMOL_MENU_APP_APLIC'        ,'Applications &amp; Invitations');
+    define('EMOL_MENU_APP_MATCH'        ,'Match profile (my wishes)');
     
     /** applicant account **/
-    define('EMOL_ACCOUNT_APP_PHOTO',    'Foto');
-    define('EMOL_ACCOUNT_APP_CV',        'Je CV document');
-    define('EMOL_ACCOUNT_APP_EMAIL',    'E-mail adres');
-    define('EMOL_ACCOUNT_APP_PHONE',    'Telefoonnummer');
-    define('EMOL_ACCOUNT_APP_CITY',        'Woonplaats');
-    define('EMOL_ACCOUNT_APP_ZIPCODE',    'Postcode');
-    define('EMOL_ACCOUNT_APP_ADDRESS',    'Adres en huisnummer');
-    define('EMOL_ACCOUNT_APP_LASTNAME',    'Achternaam');
-    define('EMOL_ACCOUNT_APP_FIRSTNAME','Voornaam');
-    define('EMOL_ACCOUNT_APP_CVTITLE',    'Titel CV');
+    define('EMOL_ACCOUNT_APP_PHOTO',    'Photo');
+    define('EMOL_ACCOUNT_APP_CV',        'Your resume');
+    define('EMOL_ACCOUNT_APP_EMAIL',    'E-mail address');
+    define('EMOL_ACCOUNT_APP_PHONE',    'Phonenumber');
+    define('EMOL_ACCOUNT_APP_CITY',        'City');
+    define('EMOL_ACCOUNT_APP_ZIPCODE',    'Zipcode');
+    define('EMOL_ACCOUNT_APP_ADDRESS',    'Street and housenumber');
+    define('EMOL_ACCOUNT_APP_LASTNAME',    'Lastname');
+    define('EMOL_ACCOUNT_APP_FIRSTNAME','Firstname');
+    define('EMOL_ACCOUNT_APP_CVTITLE',    'Title of your resume');
     
     /**global things**/
-    define('EMOL_ACCOUNT_EMPTY',        '<i>Geen omschrijving</i>');
-    define('EMOL_ACCOUNT_SAVE',            'Wijzig mijn gegevens');
-    define('EMOL_SEARCH_READMORE'       ,'lees meer >> ');
-    define('EMOL_OR'                     ,'of');
-    define('EMOL_PAGE'                     ,'Pagina: ');
-    define('EMOL_LOGGEDINAS'            ,'Ingelogd als ');
+    define('EMOL_ACCOUNT_EMPTY',        '<i>no description</i>');
+    define('EMOL_ACCOUNT_SAVE',            'Change my data');
+    define('EMOL_SEARCH_READMORE'       ,'read more >> ');
+    define('EMOL_OR'                     ,'or');
+    define('EMOL_PAGE'                     ,'Page: ');
+    define('EMOL_LOGGEDINAS'            ,'Logged on as: ');
     define('EMOL_ACCOUNT_TITLE'            ,'Account');
-    define('EMOL_GENDER'                 ,'Geslacht');
+    define('EMOL_GENDER'                 ,'Gender');
     define('EMOL_MALE'                     ,'Man');
-    define('EMOL_FEMALE'                 ,'Vrouw');
-    define('EMOL_REACT'                    ,'Reageren');
-    define('EMOL_APPLY'                    ,'Solliciteren');
-    define('EMOL_FIRSTNAME',            'Voornaam');
-    define('EMOL_MIDDLENAME',            'Tussenv.');
-    define('EMOL_LASTNAME',                'Achternaam');
-    define('EMOL_ADDRESS',                'Adres');
-    define('EMOL_HOUSENUMBER',            'huisnr.');
-    define('EMOL_EXTENSION',            'toev.');
-    define('EMOL_ZIPCODE',                'Postcode');
-    define('EMOL_CITY',                    'Woonplaats');
-    define('EMOL_PHONE',                'Telefoon');
-    define('EMOL_EMAIL',                'E-mail adres');
-    define('EMOL_ON',                    'Aan');
-    define('EMOL_OFF',                    'Uit');
+    define('EMOL_FEMALE'                 ,'Female');
+    define('EMOL_REACT'                    ,'React');
+    define('EMOL_APPLY'                    ,'Apply');
+    define('EMOL_FIRSTNAME',            'Firstname');
+    define('EMOL_MIDDLENAME',            'Middlename');
+    define('EMOL_LASTNAME',                'Lastname');
+    define('EMOL_ADDRESS',                'Address');
+    define('EMOL_HOUSENUMBER',            'housnr.');
+    define('EMOL_EXTENSION',            'exten.');
+    define('EMOL_ZIPCODE',                'Zipcode');
+    define('EMOL_CITY',                    'Place');
+    define('EMOL_PHONE',                'Phonenumber');
+    define('EMOL_EMAIL',                'E-mail address');
+    define('EMOL_ON',                    'On');
+    define('EMOL_OFF',                    'Off');
     define('EMOL_ONLINE',                'Online');
     define('EMOL_OFFLINE',                'Offline');
     
     /** login widget **/
-    define('EMOL_LOGIN_USER',            'Gebruikersnaam');
-    define('EMOL_LOGIN_PASS',            'Wachtwoord');
+    define('EMOL_LOGIN_USER',            'Username');
+    define('EMOL_LOGIN_PASS',            'Password');
     
     /** applicant registration **/
-    define('EMOL_APPLY_CV',                'CV');
-    define('EMOL_APPLY_PICTURE',        'Pasfoto');
-    define('EMOL_APPLY_MOTIVATION',        'Motivatie');
-    define('EMOL_APPLY_MATCHPROFILE',   'Uw match profiel');
-    define('EMOL_APPLY_LINKEDIN',        'Solliciteer met LinkedIn');
-    define('EMOL_APPLY_SEND',            'Sollicitatie verzenden');
+    define('EMOL_APPLY_CV',                'Resume');
+    define('EMOL_APPLY_PICTURE',        'Photo');
+    define('EMOL_APPLY_MOTIVATION',        'Motivation');
+    define('EMOL_APPLY_MATCHPROFILE',   'Your match profile');
+    define('EMOL_APPLY_LINKEDIN',        'Apply with LinkedIn');
+    define('EMOL_APPLY_SEND',            'Send your application');
     
     /** messages **/
-    define('EMOL_ACCOUNT_APP_MSG_SAVED',        'Uw nieuwe gegevens zijn verstuurd naar ons ter goedkeuring. Het kan dus even duren voordat je wijzigingen zichtbaar zijn!');
-    define('EMOL_ACCOUNT_APP_MSG_MUTATION',    'Let op: De door jou gewijzigde gegevens zijn nog niet definitief verwerkt in ons systeem. Bij je sollicitaties zullen deze wijzigingen nog niet worden doorgegeven.');
-    define('EMOL_ACCOUNT_COMP_MSG_MUTATION',    'Let op: De door u gewijzigde gegevens zijn nog niet definitief verwerkt in ons systeem.');
-    define('EMOL_APPLY_FAIL_MSG',                'Er ging iets fout tijdens uw sollicitatie, daarom is deze niet verstuurd. Probeer het alstublieft nog een keer.');
+    define('EMOL_ACCOUNT_APP_MSG_SAVED',        'Your new data is sent to us for approval. It may take some time before changes are visible!');
+    define('EMOL_ACCOUNT_APP_MSG_MUTATION',    'Note: The amended information by you are not permanently incorporated into our system. When you apply to a job these changes will not be passed.');
+    define('EMOL_ACCOUNT_COMP_MSG_MUTATION',    'Note: The modified data is not finalized and incorporated into our system.');
+    define('EMOL_APPLY_FAIL_MSG',                'Something went wrong in your application. Please try again.');
     
     /** global for applicants and companys **/
     define('EMOL_MENU_LOGIN'        ,'Login');
-    define('EMOL_MENU_LOGOUT'        ,'Uitloggen');
+    define('EMOL_MENU_LOGOUT'        ,'Logout');
     
     /**Admin area**/
     define('EMOL_ADMIN_GLOBAL',    'EazyMatch');
-    define('EMOL_ADMIN_LICENCE',    'Uw licentie');
-    define('EMOL_ADMIN_FILTERS',    'Filter vacatures op type (geen selectie = alles)');
+    define('EMOL_ADMIN_LICENCE',    'Uw Licence');
+    define('EMOL_ADMIN_FILTERS',    'Filter jobs on type (no selection = all jobs)');
     define('EMOL_ADMIN_ACCOUNT',    'Accounts');
-    define('EMOL_ADMIN_JOB',        'Vacatures');
-    define('EMOL_ADMIN_CV',        'CV\'s');
-    define('EMOL_ADMIN_SETTINGS',    'Instellingen');
-    define('EMOL_ADMIN_MSGNORESULT',    'Bericht bij geen resultaat');
-    define('EMOL_ADMIN_RESULTSPERPAGE',    'Aantal resultaten per pagina');
-    define('EMOL_ADMIN_SAVEMSG',    'Instellingen opgeslagen. LET OP: Vergeet niet uw Permalinks bij te werken wanneer u de url structuur gewijzigd heeft!');
+    define('EMOL_ADMIN_JOB',        'Jobs');
+    define('EMOL_ADMIN_CV',        'Resume\'s');
+    define('EMOL_ADMIN_SETTINGS',    'Settings');
+    define('EMOL_ADMIN_MSGNORESULT',    'Message on no results');
+    define('EMOL_ADMIN_RESULTSPERPAGE',    'Amount of results per page');
+    define('EMOL_ADMIN_SAVEMSG',    'Settings saved. NOTE: if you changed your url structure, update your permalinks!');
     
-    define('EMOL_ADMIN_JOBDISPLAY_URL', "Vacature weergave url");
-    define('EMOL_ADMIN_JOBSEARCH_URL', "Vacature zoek url");
-    define('EMOL_ADMIN_JOBSEARCH_LOGOS', "Logo's bedrijven zichtbaar?");
-    define('EMOL_ADMIN_JOBSEARCH_DESCR', "Korte omschrijving in zoekresultaat?");
-    define('EMOL_ADMIN_JOBSEARCH_REGIO', "Regio in zoekresultaat?");
-    define('EMOL_ADMIN_JOBHEADER', "Vacature kop");
-    define('EMOL_ADMIN_APPLYING', "Sollicitatie instellingen");
-    define('EMOL_ADMIN_APPLY_URL', "Sollicitatie url");
-    define('EMOL_ADMIN_APPLY_URL_FREE', "Url voor open inschrijving");
-    define('EMOL_ADMIN_MSGAFTERAPPLY', "Bericht na inschrijving/soll.");
+    define('EMOL_ADMIN_JOBDISPLAY_URL', "Job url");
+    define('EMOL_ADMIN_JOBSEARCH_URL', "Job search url");
+    define('EMOL_ADMIN_JOBSEARCH_LOGOS', "Logo's companys visible?");
+    define('EMOL_ADMIN_JOBSEARCH_DESCR', "Short description in searchresult?");
+    define('EMOL_ADMIN_JOBSEARCH_REGIO', "Region in searchresult?");
+    define('EMOL_ADMIN_JOBHEADER', "Job heading");
+    define('EMOL_ADMIN_APPLYING', "Apply settings");
+    define('EMOL_ADMIN_APPLY_URL', "Apply url");
+    define('EMOL_ADMIN_APPLY_URL_FREE', "Url for open application");
+    define('EMOL_ADMIN_MSGAFTERAPPLY', "Message after application");
     
-    define('EMOL_ADMIN_CVDISPLAY_URL', "CV weergave url");
-    define('EMOL_ADMIN_CV_PICTURE', "Pasfoto's weergeven");
-    define('EMOL_ADMIN_CV_HEADER', "CV kop");
-    define('EMOL_ADMIN_CVSEARCH_URL', "CV zoeken url");
-    define('EMOL_ADMIN_REACTING', "Reageer instellingen");
-    define('EMOL_ADMIN_REACT_URL', "Url voor reacties");
-    define('EMOL_ADMIN_REACT_FREE_URL' , "Url voor open aanmelding");
-    define('EMOL_ADMIN_MSGAFTERREACT', "Bedankt voor uw aanmelding.");
+    define('EMOL_ADMIN_CVDISPLAY_URL', "Resume url");
+    define('EMOL_ADMIN_CV_PICTURE', "Display photos");
+    define('EMOL_ADMIN_CV_HEADER', "Resume heading");
+    define('EMOL_ADMIN_CVSEARCH_URL', "Search url");
+    define('EMOL_ADMIN_REACTING', "Reacting settings (for company\'s)");
+    define('EMOL_ADMIN_REACT_URL', "Url for reactions");
+    define('EMOL_ADMIN_REACT_FREE_URL' , "Url for open registration");
+    define('EMOL_ADMIN_MSGAFTERREACT', "Thanks for your registration!");
